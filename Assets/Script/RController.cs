246eb656fe064a5d48c686e0c074f618221000da
Todah01 <wobo5695@gmail.com>
2023-04-25 14:10:52 +0900

    Initialize Oculus Rift example

---
+++ Assets/Script/RController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RController : MonoBehaviour
+{
+    public GameObject grabobj;
+    public Transform grabpos;
+
+    bool iscontact = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        iscontact = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void TouchObj()
+    {
+        OVRInput.SetControllerVibration(0.5f, 0.5f, OVRInput.Controller.RTouch);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Grabable") == true)
+        {
+            iscontact = true;
+            grabobj = other.gameObject;
+        }
+        if (other.CompareTag("Moveable") == true)
+        {
+            other.GetComponent<Rigidbody>().AddForce(OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RTouch) * 10f,ForceMode.Force);
+        }
+        TouchObj();
+    }
+}
---
+++ Assets/Script/RTouch_Control.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTouch_Control : MonoBehaviour
+{
+    public Transform GrabPos;
+
+    GameObject Grabobj;
+
+    bool isGrabed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        if (isGrabed == true)
+        {
+            //컨트롤러가 흔들림
+            Vector3 shaked = OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RTouch);
+            float speed = Vector3.SqrMagnitude(shaked); // 속도 체크 magnitude
+            if (speed >= 1.5f)
+            {
+                Grabobj.GetComponent<Rigidbody>().isKinematic = false;
+                Grabobj.transform.SetParent(null);
+                isGrabed = false;
+                Grabobj = null;
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Ball")
+        {
+            OVRInput.SetControllerVibration(0.1f, 0.5f, OVRInput.Controller.RTouch); //컨트롤러의 진동발생
+            collision.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            collision.transform.SetParent(GrabPos);
+            Grabobj = collision.gameObject;
+            isGrabed = true;
+        }
+    }
+}
